b5dbd9ca1d014cad7755d6b2a80feacab8e19d61
AD-BAYER-CNB\EGNUF <abey.john.ext@bayer.com>
2023-11-16 10:43:28 +0530

    POC-mgt-people-picker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgControl } from '@angular/forms';
+
+
+import { Providers,Msal2Provider } from "@microsoft/mgt";
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'demo-mgt-angular';
+  ngOnInit(): void {
+    Providers.globalProvider=new Msal2Provider({clientId:'57aab24e-15fd-4b67-ba5f-87023c030ba2'
+    ,scopes:['user.read,people.read']
+  });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule,CUSTOM_ELEMENTS_SCHEMA  } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
   ],
+  
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
